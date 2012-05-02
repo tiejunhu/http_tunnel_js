114cb3bebe9aa5f89db6d17991142e7f7e0d0855
oldhu <me@oldhu.com>
2012-05-02 10:54:09 +0800

    Revert to 37d6a9210fea808c1376a4d7d9b904cd7836bf8f

---
+++ mock_server.js
@@ -0,0 +1,43 @@
+var net = require('net');
+
+var config = {
+  listen_address: null,
+  port: 9100
+};
+
+function createServer() {
+  var server = net.createServer(function(socket) {
+      var size = 0;
+      socket.on('data', function(data) {
+        size += data.length;
+        socket.write(data, null);
+        console.log(data.length + ", " + size);
+      });
+  });
+  return server;
+}
+
+var server;
+
+function start(callback) {
+  server = createServer();
+  server.listen(config.port, config.listen_address, function() {
+    if (callback) {
+      callback();
+    }
+  });
+  console.log('Mock echo server running at ' + config.listen_address + ':' + config.port);
+}
+
+function stop() {
+  server.close();
+}
+
+exports.start = start;
+exports.stop = stop;
+exports.config = config;
+
+// run alone
+if (!module.parent) {
+  start();
+}
--- test.coffee
+++
@@ -1,9 +0,0 @@
-hash =
-  key1: 1
-  key2: 2
-
-console.log hash
-
-for key,value of hash
-  console.log(key)
-  console.log(value)
